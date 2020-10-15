1ab838e5f7b3a5549b59b8479f6862d7369119b4
David Edson <eddo888@tpg.com.au>
2020-10-15 15:37:06 +1100

    ok

---
+++ test/makeXMI.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys, os, re
+
+sys.path.insert(0, '..')
+
+from Perdy.parser import printXML
+from Swapsies.xmi import XMI
+
+xmi = XMI()
+
+classes = xmi.makePackage('Fundamentals', xmi.modelNS)
+diagram = xmi.makeClassDiagram('Fundamentals', classes)
+
+for base_type in ['text']:
+	_base_type = xmi.makeClass(base_type, classes, uid=base_type)
+	xmi.addDiagramClass(_base_type, diagram)
+	xmi.makeStereotype('BaseType', _base_type)
+	
+
+with open(sys.argv[0]+'.xmi', 'w') as _output:
+	printXML(str(xmi.doc), output=_output, colour=False)
+
