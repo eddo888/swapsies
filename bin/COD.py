ef4d26fbb555ea386df0bfe5a3008c2b63aa3877
David Edson <eddo888@tpg.com.au>
2020-12-04 11:04:21 +1100

    works

--- bin/COD.py
+++ bin/COD.py
@@ -70,26 +70,45 @@ class COD(object):
 			8: 'bold',
 		}
 
+	
+	#........................................................
+	def __attributes(self, cod, childItem, checkboxes):
+		font = int(getElementText(cod.ctx, 'fontStyle', childItem))
+		colour = int(getElementText(cod.ctx, 'color', childItem))
+		state = int(getElementText(cod.ctx, 'completionState', childItem)) == 3
 
+		if checkboxes:
+			checked = '✓ ' if state else '□ '
+		else:
+			checked = ''
+
+		return font, colour, state, checked
+	
+
+	#........................................................
+	def __document(self, cod, file):
+		s = getElementText(cod.ctx, '/Document/Properties/lastModificationTime')
+		u = arrow.get(float(s))
+		a = u.to('local').format('YYYY-MM-DD HH:mm:ss SSS Z')
+		t = getElementText(cod.ctx, '/Document/Properties/title')
+		print('%s -> "%s" => %s' % (file, t, a))
+
+				   
 	#........................................................
 	@logger.debug
 	def __node2text(self, cod, childItem, indent='', checkboxes=False, shownotes=False):
 		'''
 		show a single item
 		'''
-		font = int(getElementText(cod.ctx, 'fontStyle', childItem))
-		colour = int(getElementText(cod.ctx, 'color', childItem))
-		state = int(getElementText(cod.ctx, 'completionState', childItem)) == 3
-		if checkboxes:
-			checked = '[x] ' if state else '[ ] '
-		else:
-			checked = ''
 
+		font, colour, state, checked = self.__attributes(cod, childItem, checkboxes)
+		
 		if colour in self.colours.keys():
 			_colour = self.xcolours[self.colours[colour]] or ''
 		else:
 			_colour = ''
 		_font = self.xfonts[self.fonts[font]] or ''
+		
 		print('%s%s%s%s%s%s' % (
 			indent, _font, _colour, checked,
 			getElementText(cod.ctx, 'title', childItem),
@@ -123,11 +142,8 @@ class COD(object):
 		'''
 		show the cod file
 		'''
-		s = getElementText(cod.ctx, '/Document/Properties/lastModificationTime')
-		u = arrow.get(float(s))
-		a = u.to('local').format('YYYY-MM-DD HH:mm:ss SSS Z')
-		t = getElementText(cod.ctx, '/Document/Properties/title')
-		print('%s -> "%s" => %s' % (file, t, a))
+		self.__document(cod, file)
+		
 		for childItem in getElements(cod.ctx, '/Document/Properties/context/ChildItem'):
 			self.__node2text(cod, childItem, checkboxes=checkboxes, shownotes=shownotes)
 			
@@ -138,12 +154,7 @@ class COD(object):
 	@args.parameter(name='checkboxes', short='c', flag=True, help='show checkboxes')
 	@args.parameter(name='shownotes', short='n', flag=True, help='show text notes')
 	@args.parameter(name='blackAndWhite', short='b', flag=True, help='black and white')
-	def cod2text(self,
-		file,
-		checkboxes=False,
-		shownotes=False,
-		blackAndWhite=False
-	):
+	def cod2text(self, file, checkboxes=False, shownotes=False, blackAndWhite=False):
 		'''
 		load a cod file and display
 		'''
@@ -161,18 +172,12 @@ class COD(object):
 
 	#........................................................
 	@logger.debug
-	def __node2docx(self, cod, docx, childItem, depth=None, checkboxes=False, shownotes=False):
+	def __node2docx(self, cod, docx, childItem, depth=None, checkboxes=False, shownotes=False, blackAndWhite=False):
 		'''
 		show a single item
 		'''
 
-		font = int(getElementText(cod.ctx, 'fontStyle', childItem))
-		colour = int(getElementText(cod.ctx, 'color', childItem))
-		state = int(getElementText(cod.ctx, 'completionState', childItem)) == 3
-		if checkboxes:
-			checked = '[x] ' if state else '[ ] '
-		else:
-			checked = ''
+		font, colour, state, checked = self.__attributes(cod, childItem, checkboxes)
 
 		if colour in self.colours.keys():
 			_colour = self.xcolours[self.colours[colour]] or ''
@@ -184,7 +189,7 @@ class COD(object):
 		
 		text = getElementText(cod.ctx, 'title', childItem)
 		print('%s%s%s%s%s%s' % (
-			'  '*depth, _font, _colour, checked,
+			'  '*(depth-1), _font, _colour, checked,
 			text,
 			self.xcolours['Off'], )
 		)
@@ -195,12 +200,13 @@ class COD(object):
 		if font & 2: run.underline = True
 		if font & 1: run.font.strike = True
 
-		if colour == 1: run.font.color.rgb = RGBColor(0x80, 0x80, 0x80)
-		if colour == 2: run.font.color.rgb = RGBColor(0xff, 0x00, 0x00)
-		if colour == 3: run.font.color.rgb = RGBColor(0xff, 0x80, 0x00)
-		if colour == 4: run.font.color.rgb = RGBColor(0x00, 0xff, 0x00)
-		if colour == 5: run.font.color.rgb = RGBColor(0x00, 0x00, 0xff)
-		if colour == 6: run.font.color.rgb = RGBColor(0xff, 0x00, 0xff)
+		if not blackAndWhite:
+			if colour == 1: run.font.color.rgb = RGBColor(0x80, 0x80, 0x80)
+			if colour == 2: run.font.color.rgb = RGBColor(0xff, 0x00, 0x00)
+			if colour == 3: run.font.color.rgb = RGBColor(0xff, 0x80, 0x00)
+			if colour == 4: run.font.color.rgb = RGBColor(0x00, 0xff, 0x00)
+			if colour == 5: run.font.color.rgb = RGBColor(0x00, 0x00, 0xff)
+			if colour == 6: run.font.color.rgb = RGBColor(0xff, 0x00, 0xff)
 
 		if shownotes:
 			notes = getElement(cod.ctx, 'notes', childItem)
@@ -208,11 +214,11 @@ class COD(object):
 				note = notes.content
 				if note != '(null)':
 					print('%s%s  "%s"%s' % (
-						self.xcolours['Teal'], '%s  ' % ('  '*depth)
-						if checkboxes else '  '*depth, note,
+						self.xcolours['Teal'], '%s  ' % ('  '*(depth-1))
+						if checkboxes else '  '*(depth-1), note,
 						self.xcolours['Off'],
 					))
-					indent = Inches(depth*0.25)
+					indent = Inches((depth-1)*0.25)
 					docx.add_paragraph(note).paragraph_format.left_indent = indent
 
 
@@ -223,23 +229,20 @@ class COD(object):
 				grandChild,
 				depth=depth+1,
 				checkboxes=checkboxes,
-				shownotes=shownotes
+				shownotes=shownotes,
+				blackAndWhite=blackAndWhite,
 			)
 			
 
 	#........................................................
 	@logger.debug
-	def __nodes2docx(self, file, cod, docx, checkboxes=False, shownotes=False):
+	def __nodes2docx(self, file, cod, docx, checkboxes=False, shownotes=False, blackAndWhite=False):
 		'''
 		show the cod file
 		'''
-		s = getElementText(cod.ctx, '/Document/Properties/lastModificationTime')
-		u = arrow.get(float(s))
-		a = u.to('local').format('YYYY-MM-DD HH:mm:ss SSS Z')
-		t = getElementText(cod.ctx, '/Document/Properties/title')
-		print('%s -> "%s" => %s' % (file, t, a))
+		self.__document(cod, file)
 		for childItem in getElements(cod.ctx, '/Document/Properties/context/ChildItem'):
-			self.__node2docx(cod, docx, childItem, checkboxes=checkboxes, shownotes=shownotes)
+			self.__node2docx(cod, docx, childItem, checkboxes=checkboxes, shownotes=shownotes, blackAndWhite=blackAndWhite)
 			
 
 	#........................................................
@@ -248,12 +251,7 @@ class COD(object):
 	@args.parameter(name='checkboxes', short='c', flag=True, help='show checkboxes')
 	@args.parameter(name='shownotes', short='n', flag=True, help='show text notes')
 	@args.parameter(name='blackAndWhite', short='b', flag=True, help='black and white')
-	def cod2docx(self,
-		file,
-		checkboxes=False,
-		shownotes=False,
-		blackAndWhite=False
-	):
+	def cod2docx(self, file, checkboxes=False, shownotes=False, blackAndWhite=False):
 		'''
 		load a cod file and display
 		'''
@@ -267,7 +265,7 @@ class COD(object):
 			m =  p.match(style.name)
 			if m:
 				depth = int(m.group(1))
-				indent = Inches(depth*0.25)
+				indent = Inches((depth-1)*0.25)
 				style.paragraph_format.left_indent = indent
 
 		if blackAndWhite:
@@ -277,7 +275,7 @@ class COD(object):
 				self.xfonts[key] = ''
 
 		cod = XML(*getContextFromFile(os.path.expanduser(file)))
-		self.__nodes2docx(file, cod, docx, checkboxes=checkboxes, shownotes=shownotes)
+		self.__nodes2docx(file, cod, docx, checkboxes=checkboxes, shownotes=shownotes, blackAndWhite=blackAndWhite)
 
 		docx.save(f'{file}.docx')
 
